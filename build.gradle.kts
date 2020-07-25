4d35c0446477d3324f21ce8a0dbbca50ba7ac528
Smart Tool Factory <smarttoolfactory@icloud.com>
2020-07-25 13:19:26 +0300

    Add ktLint and migrate settings.gradle, and project level build.gradle to kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+
+    repositories {
+        google()
+        jcenter()
+
+        // 🔥 Groovy
+        //        maven {url "https://plugins.gradle.org/m2/"}
+
+        // 🔥 Kotlin DSL
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+    }
+
+    // 🔥 Groovy
+//    dependencies {
+//        classpath "com.android.tools.build:gradle:4.1.0-beta04"
+//        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+//
+//        // NOTE: Do not place your application dependencies here; they belong
+//        // in the individual module build.gradle files
+//
+//        classpath "org.jlleitschuh.gradle:ktlint-gradle:9.3.0"
+//
+//    }
+
+    // 🔥 Kotlin DSL
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.0-beta04")
+        classpath(kotlin("gradle-plugin", version = "1.3.72"))
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.3.0")
+    }
+}
+
+plugins {
+    id("org.jlleitschuh.gradle.ktlint").version("9.3.0")
+}
+
+allprojects {
+
+    repositories {
+        google()
+        jcenter()
+    }
+
+    // 🔥 Groovy
+//    apply plugin: "org.jlleitschuh.gradle.ktlint"
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
