b0d9f6fada3488b49eb73d01c4de0a5776c64675
Smart Tool Factory <smarttoolfactory@icloud.com>
2020-07-25 20:56:55 +0300

    Add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,26 +39,40 @@ buildscript {
 
 plugins {
     id("org.jlleitschuh.gradle.ktlint") version "9.3.0"
+    id("io.gitlab.arturbosch.detekt") version "1.10.0"
 }
 
 allprojects {
-
     repositories {
         google()
         jcenter()
     }
-
     // 🔥 Groovy
 //    apply plugin: "org.jlleitschuh.gradle.ktlint"
 }
 
 subprojects {
+
+    // KtLint
     apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
 
     // Optionally configure plugin
     ktlint {
         debug.set(true)
     }
+
+    // Detekt
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    detekt {
+        config = files("${project.rootDir}/config/detekt/detekt.yml")
+        parallel = true
+    }
+}
+
+// JVM target applied to all Kotlin tasks across all sub-projects
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
 }
 
 tasks.register("clean", Delete::class) {
